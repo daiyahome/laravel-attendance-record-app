e89435dee2f63723dc479498fe2a7688815eae1c
daiyahome <honbu@daiyahome.jp>
2025-02-17 14:53:03 +0900

    テーブルを追加

---
+++ database/migrations/2025_02_17_122643_create_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->date('date');
+            $table->string('type_name');
+            $table->foreign('type_name')->references('name')->on('types');
+            $table->datetime('starting_time');
+            $table->datetime('closing_time');
+            $table->integer('holiday_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('records');
+    }
+};
---
+++ database/migrations/2025_02_17_143203_modify_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('department_id')->constrained();
+            $table->date('start_date');
+            $table->integer('length');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('department_id');
+            $table->dropColumn('start_date');
+            $table->dropColumn('length');
+        });
+    }
+};
